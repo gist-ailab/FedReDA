dcf627dfcd7eccded57a779e7762503cda586ef4
Yunjae101 <boss9911@naver.com>
2025-11-23 17:41:02 +0000

    fix: gitignore

--- FedReDA.py
+++ FedReDA.py
@@ -123,7 +123,7 @@ def setup_logging(flag=None):
     for h in logger.handlers[:]:
         logger.removeHandler(h)
 
-    fname = 'FedDouble_KD2.txt' if flag is None else f'FedDouble_KD2_{flag}.txt'
+    fname = 'results/FedDouble_KD2.txt' if flag is None else f'results/FedDouble_KD2_{flag}.txt'
     fh = logging.FileHandler(fname, mode='a')
     fh.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
     logger.addHandler(fh)
